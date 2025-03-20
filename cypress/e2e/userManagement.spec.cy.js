a5816586418dbcc689d395ebbab3973cfd2527b7
Louis <roccalouis26@gmail.com>
2025-03-20 18:32:40 +0100

    add cypress test

--- cypress/e2e/userManagement.spec.cy.js
+++ cypress/e2e/userManagement.spec.cy.js
@@ -1,5 +1,22 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("User Management", () => {
+  beforeEach(() => {
+    // Configuration initiale et intercepteurs
     cy.visit("http://localhost:8000");
+  });
+
+  it("devrait lister les utilisateurs existants", () => {
+    // Test de lecture (GET)
+  });
+
+  it("devrait ajouter un nouvel utilisateur", () => {
+    // Test de création (POST)
+  });
+
+  it("devrait modifier un utilisateur existant", () => {
+    // Test de mise à jour (PUT)
+  });
+
+  it("devrait supprimer un utilisateur", () => {
+    // Test de suppression (DELETE)
   });
 });
